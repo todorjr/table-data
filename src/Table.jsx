bdc0905786484a21c15798bab06287c936197d0b
Lazar V. Todorovic <lazar.todorovic@jack.agency>
2024-02-15 17:49:38 +0100

    table controls

--- src/Table.jsx
+++ src/Table.jsx
@@ -3,7 +3,20 @@ function Table () {
     return (
         <div>
         <h2>Current Employees</h2>
-
+        <div className="table-controls">
+        <div className="show-entries">
+            <label htmlFor="show-by">Show by:</label>
+            <select name="show-by" id="show-by">
+                <option value="10">10</option>
+                <option value="20">20</option>
+                <option value="30">30</option>
+            </select>
+        </div>
+        <div className="search-box">
+            <input type="text" id="search" placeholder="Search..." />
+            <button type="button" id="search-btn">Search</button>
+        </div>
+    </div>
         <table>
             <tr>
                 <th>First Name</th>
