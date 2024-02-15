4059d86c93ec06f496eb4350b7b0ad9c8582ec66
Lazar V. Todorovic <lazar.todorovic@jack.agency>
2024-02-15 19:36:34 +0100

    wip employees props

--- src/Table.jsx
+++ src/Table.jsx
@@ -1,11 +1,11 @@
 
-function Table () {
+function Table (employees) {
     return (
         <div>
         <h2>Current Employees</h2>
         <div className="table-controls">
         <div className="show-entries">
-            <label htmlFor="show-by">Show by:</label>
+            <label className="show-by" htmlFor="show-by">Show by:</label>
             <select name="show-by" id="show-by">
                 <option value="10">10</option>
                 <option value="20">20</option>
@@ -18,29 +18,44 @@ function Table () {
         </div>
     </div>
         <table>
+            <thead>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Start Date</th>
+                    <th>Department</th>
+                    <th>Date of Birth</th>
+                    <th>Street</th>
+                    <th>City</th>
+                    <th>State</th>
+                    <th>Zip Code</th>
+                </tr>
+            </thead>
             <tbody>
-            <tr>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Start Date</th>
-                <th>Department</th>
-                <th>Date of Birth</th>
-                <th>Street</th>
-                <th>City</th>
-                <th>State</th>
-                <th>Zip Code</th>
-            </tr>
-            <tr>
-                <td>Lazar</td>
-                <td>Todorovic</td>
-                <td>12/07/2024</td>
-                <td>Web Development</td>
-                <td>01/04/1993</td>
-                <td>17 Rue de Paris</td>
-                <td>Paris</td>
-                <td>FR</td>
-                <td>75004</td>
-            </tr>
+                <tr>
+                    <td>Lazar</td>
+                    <td>Todorovic</td>
+                    <td>12/07/2024</td>
+                    <td>Web Development</td>
+                    <td>01/04/1993</td>
+                    <td>17 Rue de Paris</td>
+                    <td>Paris</td>
+                    <td>FR</td>
+                    <td>75004</td>
+                </tr>
+                  {/* {employees.map((employee, index) => (
+                        <tr key={index}>
+                            <td>{employee.firstName}</td>
+                            <td>{employee.lastName}</td>
+                            <td>{employee.startDate}</td>
+                            <td>{employee.department}</td>
+                            <td>{employee.birthDate}</td>
+                            <td>{employee.street}</td>
+                            <td>{employee.city}</td>
+                            <td>{employee.state}</td>
+                            <td>{employee.zipCode}</td>
+                        </tr>
+                    ))} */}
             </tbody>
         </table>
         </div>
