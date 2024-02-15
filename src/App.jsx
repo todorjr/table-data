30026e065fb9544092f6838ab2cc3d49236400dd
Lazar V. Todorovic <lazar.todorovic@jack.agency>
2024-02-15 17:34:26 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Table from "./Table"
+import './App.css'
+
+function App() {
+
+  return (
+    <Table />
+  )
+}
+
+export default App
---
+++ src/Table.jsx
@@ -0,0 +1,35 @@
+
+function Table () {
+    return (
+        <div>
+        <h2>Current Employees</h2>
+
+        <table>
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Start Date</th>
+                <th>Department</th>
+                <th>Date of Birth</th>
+                <th>Street</th>
+                <th>City</th>
+                <th>State</th>
+                <th>Zip Code</th>
+            </tr>
+            <tr>
+                <td>Lazar</td>
+                <td>Todorovic</td>
+                <td>01/18/2024</td>
+                <td>Engineering</td>
+                <td>01/17/2024</td>
+                <td>17 Rue de Paris</td>
+                <td>Paris</td>
+                <td>AK</td>
+                <td>123456</td>
+            </tr>
+        </table>
+        </div>
+    )
+}
+
+export default Table
