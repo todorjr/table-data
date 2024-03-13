93d023786f98f5a3087bb1b2e1903e320f6dd1ee
Lazar V. Todorovic <lazar.todorovic@jack.agency>
2024-03-13 20:07:14 +0100

    export table

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Table from './components/Table.jsx'
+export default Table
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+
